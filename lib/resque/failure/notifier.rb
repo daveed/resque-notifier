fb8b15df6ff12c84424972f2af04599757db2e94
Doug Wright <dwright@takepart.com>
2016-08-31 14:05:33 -0700

    hound updates

--- lib/resque/failure/notifier.rb
+++ lib/resque/failure/notifier.rb
@@ -1,6 +1,6 @@
-require 'uri'
-require 'resque'
-require 'net/http'
+require "uri"
+require "resque"
+require "net/http"
 
 module Resque
   module Failure
@@ -9,17 +9,19 @@ module Resque
 
       def initialize(exception, worker, queue, payload)
         @error_exception = (exception && exception.class).to_s
-        @error_class     = (payload && payload['class']).to_s
+        @error_class     = (payload && payload["class"]).to_s
         super
       end
 
       def save
         unless exception_logged_already?
-          uri     = URI(ENV['RESQUE_HOOK'])
-          payload = {channel:    ENV['RESQUE_CHANNEL'],
-                     username:   "resque",
-                     text:       text,
-                     icon_emoji: ":ghost:"}.to_json
+          uri     = URI(ENV["RESQUE_HOOK"])
+          payload = {
+            channel:    ENV["RESQUE_CHANNEL"],
+            username:   "resque",
+            text:       text,
+            icon_emoji: ":ghost:"
+          }.to_json
           Net::HTTP.post_form(uri, payload: payload)
         end
       end
@@ -40,9 +42,9 @@ module Resque
       end
 
       def text
-        "Worker: #{worker.to_s}       \n" +
-        "Class: #{@error_class}        \n" +
-        "Exception: #{@error_exception} \n" +
+        "Worker: #{worker.to_s}       \n" \
+        "Class: #{@error_class}        \n" \
+        "Exception: #{@error_exception} \n" \
         "Error: #{exception.to_s}        \n"
       end
 
